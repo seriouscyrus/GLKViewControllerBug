f51d03d2170580aff7a7812595e8fd65ae020474
georgebrown <cyrus_b_uk@server>
2017-10-19 16:24:11 +0200

    Added the update method
    update method is called, only the drawInRect method is not called.

--- SCGLKViewController.m
+++ SCGLKViewController.m
@@ -14,6 +14,20 @@
 {
     [super viewDidLoad];
     NSLog(@"View loaded");
+    // Uncomment to assign the GLKViewController Delegate
+//    self.delegate = self;
+    NSLog(@"ViewController delegate is self? %@", self.delegate == self ? @"YES" : @"NO");
+
+    if (self.view) {
+        GLKView *view = (GLKView *)self.view;
+        NSLog(@"View delegate is self? %@", view.delegate == self ? @"YES" : @"NO");
+    }
+}
+
+// Comment out if you want to use the GLKViewControllerrDelegate method
+- (void) update
+{
+    NSLog(@"Update called");
 }
 
 - (void) glkViewControllerUpdate:(GLKViewController *)controller
